1f4f094a97aa7771d7c9e631624877bf146277e7
unknown <justinesaburao12@gmail.com>
2022-06-01 20:52:15 +0800

    Final Pushing

---
+++ ecomweb/cart/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from .cart import Cart
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return render(request, 'carts/menu_cart.html')
